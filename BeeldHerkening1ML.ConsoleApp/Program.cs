67d1c1b8328a443d5deff923446cc73e07ba5063
Rachana Ghiraw <109738@st-maartenscollege.nl>
2020-12-16 14:37:02 +0100

    ImagesSource aangepast

--- BeeldHerkening1ML.ConsoleApp/Program.cs
+++ BeeldHerkening1ML.ConsoleApp/Program.cs
@@ -11,7 +11,8 @@ namespace BeeldHerkening1
             Debug.WriteLine(" Object is not valid for this category.");
             // Add input data
             var input = new ModelInput();
-            input.ImageSource = "C:\\Users\\pc-5\\Downloads\\Object-recognition-CIFAR-10-master\\rocva ml.NET images\\autos\\auto1.jpg";
+            input.ImageSource = @"C:\Users\racha\Downloads\Machine Learning\images\Auto\Auto1.jpg";
+
 
             // Load model and predict output of sample data
             ModelOutput result = ConsumeModel.Predict(input);
